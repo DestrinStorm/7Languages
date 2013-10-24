203324132acee59630023c114b6358eae3ae0f24
DestrinStorm <destrin@gmail.com>
2013-10-24 11:07:58 +0100

    Starting prolog

---
+++ Prolog/day1/day1.pl
@@ -0,0 +1,20 @@
+wrote('peter f hamilton', 'fallen dragon').
+wrote('terry pratchett', 'guards! guards!').
+wrote('iain m banks', 'excession').
+wrote('iain m banks', 'use of weapons').
+wrote('iain m banks', 'player of games').
+wrote('peter f hamilton', 'pandoras star').
+wrote('peter f hamilton', 'judas unchained').
+
+plays('Dave Grohl','drums').
+plays('Mike Portnoy', 'drums').
+plays('Jorden Rudess', 'keyboard').
+plays('Eric Clapton', 'guitar').
+plays('Mark Knopfler', 'guitar').
+plays('B.B. King', 'guitar').
+playsgenre('Mike Portnoy', 'Progressive Metal').
+playsgenre('Jorden Rudess', 'Progressive Metal').
+playsgenre('Eric Clapton', 'Blues').
+playsgenre('B.B. King', 'Blues').
+playsgenre('Dave Grohl', 'Rock').
+playsgenre('Mark Knopfler', 'Rock').
